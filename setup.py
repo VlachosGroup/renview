bc320c8f0b2f56dbbbc84a9301fe0b235375cc8b
Udit Gupta <uditgupta0912@gmail.com>
2019-07-12 12:51:10 -0400

    distribution and setup files

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU Lesser GPL v3 License',   # Again, pick a license
+    'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
